1006bc1269a784c0e991f833e5193d0de8c3a470
Sasank Sekhar Panda <sasanksekhar.ab@gmail.com>
2016-08-07 08:31:45 -0700

    Create HackerRank_WarmUp_CompareTheTriplets.java

---
+++ HackerRank_WarmUp_CompareTheTriplets.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a0 = in.nextInt();
+        int a1 = in.nextInt();
+        int a2 = in.nextInt();
+        int b0 = in.nextInt();
+        int b1 = in.nextInt();
+        int b2 = in.nextInt();
+        int alice=0,bob=0;
+        if(a0<b0)
+            bob++;
+        else if(a0>b0)
+            alice++;
+        if(a1<b1)
+            bob++;
+        else if(a1>b1)
+            alice++;
+        if(a2<b2)
+            bob++;
+        else if(a2>b2)
+            alice++;
+            System.out.println(alice+" "+bob);
+        
+    }
+}
